7e60c53540b26f308a8de1f0c82cb7c536f5feb3
Raman-120 <ramanchaulagain059@gmail.com>
2026-06-28 16:01:45 +0545

    set up the employee class

--- CRUD OPERATIONS/src/Employee.java
+++ CRUD OPERATIONS/src/Employee.java
@@ -1,2 +1,24 @@
 public class Employee {
+    private int employeeIdNo;
+    private String employeeName;
+    private double employeeSalary;
+
+    Employee(int employeeIdNo,String employeeName, double employeeSalary ){
+        this.employeeIdNo = employeeIdNo;
+        this.employeeName = employeeName;
+        this.employeeSalary = employeeSalary;
+    }
+
+    public int getEmployeeId(){
+        return this.employeeIdNo;
+    }
+
+    public String getEmployeeName(){
+        return this.employeeName;
+    }
+
+    public double getEmployeeSalary(){
+        return this.employeeSalary;
+    }
+
 }
